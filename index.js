96a72d0259cab484785c0e40d74b394b518e2066
tunderdomb <tunderdomb@gmail.com>
2015-10-27 14:43:29 +0100

    missclick api

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var dom = module.exports = {}
 
 dom.attributes = require("./attributes")
 dom.delegate = require("./delegate")
+dom.missclick = require("./missclick")
 dom.Fragment = require("./Fragment")
 dom.Selector = require("./Selector")
